5f4580d9de5b169e4870b1faabc583c0f03f80d6
kpowers2000 <82614160+kpowers2000@users.noreply.github.com>
2021-06-23 15:12:43 -0400

    Update TryMe_InteractiveIrreducibilityChecker.py

--- Math401Project/TryMe_InteractiveIrreducibilityChecker.py
+++ Math401Project/TryMe_InteractiveIrreducibilityChecker.py
@@ -3,23 +3,37 @@ Created on Jun 23, 2021
 
 @author: keenan
 '''
-from finitefield import isIrreducible
+from polynomial import *
+from modp import *
+from finitefield import *
 
 
 def main():
-    poly = input("Hello, I hope you are doing well! My name is Reducibility Checker, and I am here to help you find out if your polynomial is reducible or not. Let's get to it!  Can you enter the polynomial you wish to check:")
-    field = input("Great! Could you also tell me characteristic of the field.  If you do not know this please type 'Help':")
+    isYes = raw_input("Hello, I hope you are doing well! \nMy name is Reducibility Checker, and I am here to help \nIs your polynomial reducible? Let's find out! \nAre you ready to get started?  \nType 'yes' if you are. ")
+    
+    if isYes.lower() == 'yes':
+        poly = raw_input("Hooray!  Please type the coefficients of your polynomial in increasing degree with a space in between each letter. \nFor example 1 2 0 3 equates to 1 + 2x + 3x^4 ")
+    else: 
+        print("Okay!  Sorry I could not be of more service to you.")
+    field = raw_input("Great! Could you also tell me characteristic of the field. \nIf you do not know this please type 'help': ")
     print("Working my magic...")
-    '''TODO: If characteristic of the field is 0'''
-    if field.lower() == "help":
-        print("In mathematics, the characteristic of a ring R, often denoted char(R), is defined to be the smallest number of times one must use the ring's multiplicative identity (1) in a sum to get the additive identity (0). If this sum never reaches the additive identity the ring is said to have characteristic zero.")
-    canReduce = isIrreducible(poly, field)
-    if canReduce == 1:
-        print("Oops! Looks like the polynomial you entered can be reduced. Keep trying!")
-    elif canReduce == 0:
+    if field.lower() == 'help':
+        print("In mathematics, the characteristic of a ring R (often denoted char(R)), \nis defined to be the smallest number of times one must use the ring's \nmultiplicative identity (1) in a sum to get the additive identity (0). \nIf this sum never reaches the additive identity the ring is \nsaid to have characteristic zero.")
+        return
+    else:
+        field2 = int(field)
+    Zp = IntegersModP(field2)
+    Polynomial = polynomialsOver(Zp)
+    coefficients = [Zp(s) for s in poly.split(' ')]
+    poly = Polynomial(coefficients)
+    print(poly)
+    
+    irreducible = isIrreducible(poly, field2)
+    if irreducible:
         print("Hooray! The polynomial you entered is irreducible!")
-    if canReduce == -1:
-        print("Error")
+    else:
+        
+        print("Oops! Looks like the polynomial you entered can be reduced. Keep trying!")
     return
 
 
